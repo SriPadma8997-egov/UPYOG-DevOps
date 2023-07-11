1f5cde7e82cb0611903da728a4e74703579b9ed1
nikhilmulinti-egov <nikhil.mulinti@egovernments.org>
2023-07-11 10:54:25 +0530

    two job kubectl config

--- infra-as-code/terraform/sample-aws/variables.tf
+++ infra-as-code/terraform/sample-aws/variables.tf
@@ -5,7 +5,7 @@
 
 variable "cluster_name" {
   description = "Name of the Kubernetes cluster"
-  default = <cluster_name> #REPLACE
+  default = "githubactions" #REPLACE
 }
 
 variable "vpc_cidr_block" {
@@ -47,25 +47,25 @@ variable "number_of_worker_nodes" {
 
 variable "ssh_key_name" {
   description = "ssh key name, not required if your using spot instance types"
-  default = <ssh_key_name> #REPLACE
+  default = "githubactions" #REPLACE
 }
 
 
 variable "db_name" {
   description = "RDS DB name. Make sure there are no hyphens or other special characters in the DB name. Else, DB creation will fail"
-  default = <db_name> #REPLACE
+  default = "githubactions" #REPLACE
 }
 
 variable "db_username" {
   description = "RDS database user name"
-  default = <db_username> #REPLACE
+  default = "githubactions" #REPLACE
 }
 
 #DO NOT fill in here. This will be asked at runtime
 variable "db_password" {}
 
 variable "public_key" {
-  default = <public_ssh_key>
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC45oQnyw/dKJcYHeDj4oCy0Dsvxiyc8lCrX9ISZU9FD63sl/J/UBiWdPe6tBfAEsShGWOnaifKbHDVa+LZcs5rhCNXOK91XtJaL5uHcG8kpsL3hbr+vhZrzpHkG03ZxcnMs56NOb2uoiJ7Z37UxdWc3el9ajZidGT6/eiC6vvJYntxjlm0JYc5sPg0+XTigBihbvHMoDUwU+L9xGTJ094dhFZvZWU8oz+zrO5jDdfUEGm73oiO4i0NzaE2BETubMeRkR5NmJiYtin+EYheVEj7PDsYwB10w3bfIZVyOJ+pYnoDwyfV+4ODWc9Fds8WuCGsJwkV/dQdzPxOP08atPt9"
   description = "ssh key"
 }
 
